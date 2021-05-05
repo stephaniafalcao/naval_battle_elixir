cd7c32defff857e3acbf0d5f90c0d60895c2ea32
Stephania Nascimento <stephania.nascimento@pravaler.com.br>
2021-05-05 12:46:54 -0300

    Create the first version of a naval battle with multiple attempts

---
+++ lib/naval.ex
@@ -0,0 +1,31 @@
+defmodule NavalBattle do
+  def creatingBoard do
+		Map.new(Enum.map(1..10, fn x ->
+				{x , definingPositions()}
+			end))
+
+    IO.puts("Board Created")
+	end
+
+	def playing(board, row, column) do
+    board
+    |> Map.get(row)
+    |> Map.get(column)
+    |> printResult
+	end
+
+	defp definingPositions do
+		values = [0, 1]
+
+	  line = Enum.map(1..10, fn x ->
+			{x , Enum.random(values)}
+
+    end)
+
+		Map.new(line)
+	end
+
+  defp printResult(1), do: "You got me"
+  defp printResult(0), do: "Keep trying"
+
+end
---
+++ test/naval_test.exs
@@ -0,0 +1,8 @@
+defmodule NavalTest do
+  use ExUnit.Case
+  doctest Naval
+
+  test "greets the world" do
+    assert Naval.hello() == :world
+  end
+end
